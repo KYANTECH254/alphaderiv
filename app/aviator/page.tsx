6840bb7ad0a5f1fbc3ec08fa853feeb5f0337d15
KyanTech <kyantech254@gmail.com>
2025-04-18 11:53:43 -0400

    update

--- app/aviator/page.tsx
+++ app/aviator/page.tsx
@@ -3,9 +3,19 @@
 import CommingSoon from "@/components/CommingSoon";
 import Sidebar from "@/components/SideBar";
 import { useGetQueryParams } from "@/hooks/useGetQueryParams";
+import { useEffect, useState } from "react";
 
 export default function Page() {
+    const [isClient, setIsClient] = useState(false);
     const { token } = useGetQueryParams();
+
+    useEffect(() => {
+        setIsClient(true);
+    }, []);
+
+    if (!isClient) {
+        return null; 
+    }
 
     return (
         <Sidebar token={token}>
